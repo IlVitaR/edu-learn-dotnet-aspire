430642f02cfa459445aadc59cea9224cded412d6
FtarG <ilvitar@gmail.com>
2024-07-05 16:19:18 +0300

    Component usage example with the Redis cache (uses Docker)

--- start-with-api/AppHost/Program.cs
+++ start-with-api/AppHost/Program.cs
@@ -1,6 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var api = builder.AddProject<Projects.Api>("weather-api");
-builder.AddProject<Projects.MyWeatherHub>("web-frontend").WithReference(api);
+var cache = builder.AddRedis("output-cache");
+
+var api = builder.AddProject<Projects.Api>("weather-api")
+    .WithReference(cache);
+builder.AddProject<Projects.MyWeatherHub>("web-frontend")
+    .WithReference(api);
 
 builder.Build().Run();
